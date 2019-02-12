2f8a82d5aaac6d401eaba5d6992eb5b1f3000a5d
ksteimel <ksteimel@iu.edu>
2019-02-12 11:52:33 -0500

    Added a basic file to work with

---
+++ demo_file.py
@@ -0,0 +1,6 @@
+from __future__ import print_function
+#The above line is just for python2 compatibility. 
+
+words = ["hey", "you're","learnin","git","ya","know"]
+word_index = 3
+print(words[word_index])
